9a414f616fb8422a6a43017b7c68ba3a87bb6187
Sohail khan <skddn00786@gmail.com>
2025-01-13 22:39:25 +0530

    Shared preferences

--- android/app/src/main/kotlin/com/example/task_todo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/task_todo/MainActivity.kt
@@ -1,5 +1,3 @@
 package com.example.task_todo
 
-import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
--- lib/providers/theme_provides.dart
+++ lib/providers/theme_provides.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier{
 
   bool _isDark = false;
+  SharedPreferences?prefs;
 
-  void changeTheme(bool value){
+  void changeTheme(bool value) async{
     _isDark = value;
+    prefs =await SharedPreferences.getInstance();
+    prefs!.setBool('theme', _isDark);
     notifyListeners();
   }
 
   bool getTheme() => _isDark;
 
-
+ void getInitialTheme() async{
+   prefs = await SharedPreferences.getInstance();
+    _isDark =  prefs!.getBool('theme')!;
+    notifyListeners();
+ }
 
 
 }
